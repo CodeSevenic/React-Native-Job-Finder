6b40d352b3bef7623b5f9eaf52223d3d4e959808
CodeSevenic <sevenicbookings@gmail.com>
2023-11-09 14:56:06 +0200

    React Native Job Finder

--- app/index.js
+++ app/index.js
@@ -2,6 +2,9 @@
 import { View, Text, ScrollView, SafeAreaView } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
+
 const Home = () => {
   return (
     <View>
