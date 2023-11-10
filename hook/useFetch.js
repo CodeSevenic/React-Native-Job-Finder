3e44d97927b675ed6db542292d2bb120201b05c7
CodeSevenic <sevenicbookings@gmail.com>
2023-11-10 14:05:36 +0200

    React Native Code Update

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -4,7 +4,7 @@ import { RAPID_API_KEY } from '@env';
 
 const rapidApiKey = RAPID_API_KEY;
 
-export const useFetch = (endpoint) => {
+export const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -17,5 +17,21 @@ export const useFetch = (endpoint) => {
       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
     },
     params: { ...query },
+  };
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.request(options);
+      setData(response.data.data);
+
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
   };
 };
