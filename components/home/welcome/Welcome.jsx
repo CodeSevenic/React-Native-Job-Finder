20fd3762fb6c73bcd556ed35250cffecdef186f8
CodeSevenic <sevenicbookings@gmail.com>
2023-11-10 12:53:39 +0200

    React Native Code Update

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -49,6 +49,8 @@ const Welcome = () => {
             );
           }}
           keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
         />
       </View>
     </View>
