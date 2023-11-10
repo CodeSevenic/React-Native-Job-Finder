f3bbf8c97d730d8609615965f8e86f5342b9e07d
CodeSevenic <sevenicbookings@gmail.com>
2023-11-10 13:15:34 +0200

    React Native Code Update

---
+++ hook/useFetch.js
@@ -0,0 +1,8 @@
+﻿import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const useFetch = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+};
