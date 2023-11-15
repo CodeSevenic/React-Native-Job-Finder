c5c380a490621a3c359c938be40bfe45eaad6016
CodeSevenic <sevenicbookings@gmail.com>
2023-11-15 10:35:13 +0200

    React Native Job Finder

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -22,11 +22,32 @@ import useFetch from '../../hook/useFetch';
 
 const JobDetails = () => {
   const params = useGlobalSearchParams();
+  console.log(params.id);
   const router = useRouter();
-  const { data, isLoading, error, refetch } = useFetch(`jobs-details`, {
-    job_id: params.id,
-  });
-  return <Text>JobDetails</Text>;
+  // const { data, isLoading, error, refetch } = useFetch(`jobs-details`, {
+  //   job_id: params.id,
+  // });
+
+  console.log('Job Details Component rendered');
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension="60%"
+              handlePress={() => router.back()}
+            />
+          ),
+        }}
+      ></Stack.Screen>
+    </SafeAreaView>
+  );
 };
 
 export default JobDetails;
