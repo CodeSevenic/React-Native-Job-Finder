8bc73312db2abf362155f5996b12b51ed6db144f
CodeSevenic <sevenicbookings@gmail.com>
2023-11-15 10:12:41 +0200

    React Native Job Finder

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,5 +1,23 @@
-﻿import React from 'react';
-import { Text } from 'react-native';
+﻿import React, { useCallback, useState } from 'react';
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from 'react-native';
+import { Stack, useRouter, useSearchParams } from 'expo-router';
+
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from '../../components';
+import { COLORS, icons, SIZES } from '../../constants';
 
 const JobDetails = () => {
   return <Text>JobDetails</Text>;
