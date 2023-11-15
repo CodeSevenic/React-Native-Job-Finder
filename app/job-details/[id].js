4b36dc65737b63498276b6dec353b80f1c1a0886
CodeSevenic <sevenicbookings@gmail.com>
2023-11-15 10:15:41 +0200

    React Native Job Finder

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -18,6 +18,7 @@ import {
   Specifics,
 } from '../../components';
 import { COLORS, icons, SIZES } from '../../constants';
+import useFetch from '../../hook/useFetch';
 
 const JobDetails = () => {
   return <Text>JobDetails</Text>;
